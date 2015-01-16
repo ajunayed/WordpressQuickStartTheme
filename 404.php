b8b05aa915047dcd2434acffddc1f4c1e7ecf888
ajunayed <ajunayed@yahoo.com>
2015-01-16 11:28:54 +0600

    Create 404.php

---
+++ 404.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+
+    <!-- Maincontent -->
+    <section class="maincontent_area internal_page">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8">
+                    <?php get_template_part('includes/four-zero-four'); ?>
+
+                </div>
+
+                <?php get_sidebar(); ?>
+            </div>
+        </div>
+    </section>
+    <!-- End -->
+
+    <!-- Footer -->
+    <?php get_footer(); ?>
