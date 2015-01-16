f680d9dfb43a8f29dff7d56348741e996fa0c08e
ajunayed <ajunayed@yahoo.com>
2015-01-16 12:55:18 +0600

    Create sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,15 @@
+<div class="col-lg-4">
+    <div class="sidebar">
+        <aside>
+            <?php if ( ! dynamic_sidebar( 'right_sidebar' ) ) : ?>
+            <?php endif; ?>
+           
+
+           
+
+           
+        </aside>
+    </div>
+</div>
+
+
